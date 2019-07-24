96ed5f8834f887407427915f6a5e73fe7a64fc3a
n-designdev <49777717+n-designdev@users.noreply.github.com>
2019-07-24 19:44:54 +0900

    first commit

---
+++ export.cmd
@@ -0,0 +1,6 @@
+
+pushd C:\Users\k_ueda\Desktop\shotmenu__
+
+"C:\Program Files\Shotgun\Python\python.exe" main.py "%1"
+
+popd
---
+++ export_test.cmd
@@ -0,0 +1,6 @@
+
+pushd P:\Project\mem2\Library\Tool\maya\scripts\python\charaExporter_old
+
+"C:\Program Files\Shotgun\Python\python.exe" main2.py "%1"
+
+popd
